d9d345f16749147ca3a262a8f322bb48efbb8315
ben nelson <ben.volatic@gmail.com>
2025-06-04 10:22:36 -0700

    Compress images for faster loading

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // Import only existing images
 import Sauerseattleplungepool from "../assets/Sauerseattleplungepool.jpg";
